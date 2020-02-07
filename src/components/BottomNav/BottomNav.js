45a119ad2ec9480744840989d6f26bad7452c797
reginapizza <reginaelyse@gmail.com>
2020-02-07 14:30:40 -0500

    Fixed inconsistent spacing on bottom nav

--- src/components/BottomNav/BottomNav.js
+++ src/components/BottomNav/BottomNav.js
@@ -5,10 +5,10 @@ import Navbar from 'react-bootstrap/Navbar'
 const BottomNav = ({ user }) => (
   <Navbar bg="primary" fixed="bottom" position="fixed" className="bottomNav">
     <div className="row">
-      <Nav.Link className="col-3 link" href="#home"> Home</Nav.Link>
-      <Nav.Link className="col-3 link" href="#start">Start</Nav.Link>
-      <Nav.Link className="col-3 link" href="#user">User</Nav.Link>
-      <Nav.Link className="col-3 link" href="#resources">Resources</Nav.Link>
+      <Nav.Link className="link" href="#home"> Home</Nav.Link>
+      <Nav.Link className="link" href="#start">Start</Nav.Link>
+      <Nav.Link className="link" href="#user">User</Nav.Link>
+      <Nav.Link className="link" href="#resources">Resources</Nav.Link>
     </div>
   </Navbar>
 )
