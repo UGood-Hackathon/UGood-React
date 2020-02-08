2346623db03756bffa105ab9a0fb2896a0d0d950
reginapizza <reginaelyse@gmail.com>
2020-02-08 15:38:55 -0500

    Added phone to signup credentials
    Fixed missing period in instructions text on start page

--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -4,7 +4,7 @@ import { Button } from 'react-bootstrap'
 const Start = (props) => (
   <Fragment className="page">
     <h1 className="page-headers">Getting Started</h1>
-    <p className="instructions"> In order to get started with our service, make an account and then press the button below to get started. You will recieve a text from us with next steps</p>
+    <p className="instructions"> In order to get started with our service, make an account and then press the button below to get started. You will recieve a text from us with next steps.</p>
     <Button className="start-button" href="">Let&apos;s Start!</Button>
     <p className="disclaimer"> Please note that standard messaging rates may apply.</p>
   </Fragment>
